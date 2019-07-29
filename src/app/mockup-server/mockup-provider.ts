118f67ed231976f779e549484d94d2593ec3e845
jackiewang5566 <jackiewang5566@hotmail.com>
2019-07-28 22:57:22 -0400

    add title on each row content

--- src/app/mockup-server/mockup-provider.ts
+++ src/app/mockup-server/mockup-provider.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpResponse, HttpInterceptor, HttpEvent, HTTP_INTERCEPTORS, HttpHandler} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import data from './api/sample_data.json';
 
 
 @Injectable()
 export class MockupBackendInterceptor implements HttpInterceptor {
     constructor() {}
 
-    private _data = require('./api/sample_data.json');
+    // private _data = require('./api/sample_data.json');
+    private _data = data;
     private _result = require('./api/submit.json');
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return of (null).pipe(() => {
